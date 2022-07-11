23d8fdc8ef24947dab51efa54b1c791fde340a19
mariaasaavedra <78118659+mariaasaavedra@users.noreply.github.com>
2022-07-11 11:20:01 -0500

    ajustes

--- scripts/cook_county_.R
+++ scripts/cook_county_.R
@@ -10,9 +10,18 @@ rm(list=ls())
 
 # Directory
 
-setwd("C:\Users\BIBIANA\Desktop\Gambling")
+setwd("C:/Users/bpvel/Desktop/Analysis")
 
 # Packages required
+
+  # install.packages("rgdal")
+  # install.packages("RSocrata")
+  # install.packages("acs")
+  # install.packages("viridis")
+  # install.packages("magrittr")
+  # install.packages("tidycensus")
+  # install.packages("readxl")
+
 
 require("tidycensus", quietly = TRUE)
 require("sf", quietly = TRUE)
@@ -25,33 +34,28 @@ library(rgdal)
 library(sf)
 library(dplyr)
 
-  # install.packages("rgdal")
-  # install.packages("RSocrata")
-  # install.packages("read.socrata")
-  # install.packages("acs")
-  # install.packages("get_acs")
-  # install.packages("viridis")
-  # install.packages("magrittr")
-  # install.packages("tidycensus")
-
 
 
 # Reading Chicago data with RSocrata
 
-chicago <- RSocrata::read.socrata(
-  "https://data.cityofchicago.org/resource/bt9m-d2mf.json",
-  app_token = "L7sBjYbx4MH0hUCYCU5BESLIF",
-  email = "srmntbr2@illinois.edu",
-  password = ":Gvjj[obeP6a"
-)
+  # chicago <- RSocrata::read.socrata(
+    # "https://data.cityofchicago.org/resource/bt9m-d2mf.json",
+    # app_token = "L7sBjYbx4MH0hUCYCU5BESLIF",
+    # email = "srmntbr2@illinois.edu",
+    #password = ":Gvjj[obeP6a"
+    #)
 
-saveRDS(object = chicago, file = "chicago.rds")
+  # saveRDS(object = chicago, file = "chicago.rds")
+
+chicago <- readRDS("chicago.rds")
 
 # Charging tidycensus
 
-tidycensus <- tidycensus::census_api_key("8337396f6389748bdff0624f2dcd6f1ce10e3113", install =T, overwrite = T)
+  # tidycensus_ <- tidycensus::census_api_key("8337396f6389748bdff0624f2dcd6f1ce10e3113", install =T, overwrite = T)
 
-saveRDS(object = tidycensus, file = "tidy_api_key.rds")
+  # saveRDS(object = tidycensus_, file = "tidy_api_key.rds")
+
+tidycensus <- readRDS("tidy_api_key.rds")
 
 # Getting blockgroups information
 
@@ -72,20 +76,20 @@ plot(cook_bg)
 
   #setwd("/Users/iggy/Library/CloudStorage/OneDrive-UniversidaddelosAndes/Ignacio-Sarmiento/Gambling/")
 
-sides <- readxl::read_excel("Analysis/Data/community_areas_sides.xlsx")
+sides <- readxl::read_excel("Data/community_areas_sides.xlsx")
 
 
 # Boundaries - Census Blocks - 2010 - Downloaded on 02/06/22 from:
   # https://data.cityofchicago.org/Facilities-Geographic-Boundaries/Boundaries-Census-Blocks-2010/mfzt-js4n
 
-blocks_shp <- read_sf("Analysis/Data/Boundaries - Census Blocks - 2010/
+blocks_shp <- read_sf("Data/Boundaries - Census Blocks - 2010/
                       geo_export_fb4ce0e7-7a9a-4f5d-9b88-961cb798be53.shp")
 
 
 # Boundaries - Community Areas - 2010 - Downloaded on 30/06/22 from: 
   # https://data.cityofchicago.org/Facilities-Geographic-Boundaries/Boundaries-Community-Areas-current-/cauq-8yn6
 
-community_areas_shp <- read_sf("Analysis/Data/Boundaries - Community Areas/
+community_areas_shp <- read_sf("Data/Boundaries - Community Areas/
                                geo_export_3646c425-a3eb-42a1-964a-87e800c0e605.shp")
 
 #---------------------------------------------------------------------------------------------------------------#
@@ -108,6 +112,7 @@ join_map <- st_join(blocks_shp, community_areas_sides, join = st_intersects)
 st_crs(join_map) <- 4269
 
 final_join <- st_join(join_map, blockgroups, join = st_intersects)
+
 
 #---------------------------------------------------------------------------------------------------------------#
 
